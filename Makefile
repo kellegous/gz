f5297c9da534042ee2a3f0d5535714f08c200dbf
Kelly Norton <kellegous@gmail.com>
2025-11-26 18:10:50 -0500

    protos

--- Makefile
+++ Makefile
@@ -1,9 +1,32 @@
+PROTOC_GEN_GO_VERSION := v1.36.10
+PROTOC_VERSION := 33.0
+
+GO_MOD = $(shell go list -m)
+
+PROTOS := gz.pb.go
+
 .PHONY: all clean
+
+.PRECIOUS: $(PROTOS)
 
 all: bin/gz
 
-bin/%: cmd/%/main.go $(shell find internal -type f -name '*.go')
+bin/%: cmd/%/main.go $(PROTOS) $(shell find internal -type f -name '*.go')
 	go build -o $@ ./cmd/$*
 
+bin/protoc:
+	etc/download-protoc $(PROTOC_VERSION)
+
+bin/protoc-gen-go:
+	GOBIN="$(CURDIR)/bin" go install google.golang.org/protobuf/cmd/protoc-gen-go@$(PROTOC_GEN_GO_VERSION)
+
+
+%.pb.go: %.proto bin/protoc-gen-go bin/protoc
+	bin/protoc --proto_path=. \
+		--plugin=protoc-gen-go=bin/protoc-gen-go \
+		--go_out=. \
+		--go_opt=module=$(GO_MOD) \
+		$<
+
 clean:
-	rm -rf bin
+	rm -rf bin $(PROTOS)
--- internal/cmd/create_cmd.go
+++ internal/cmd/create_cmd.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 
 	git "github.com/go-git/go-git/v6"
 	"github.com/kellegous/poop"
@@ -10,9 +11,10 @@ import (
 
 func createCmd(rf *rootFlags) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "create",
-		Short: "create a new stacked feature branch",
-		Args:  cobra.RangeArgs(1, 2),
+		Use:     "create",
+		Short:   "create a new stacked feature branch",
+		Args:    cobra.RangeArgs(1, 2),
+		Aliases: []string{"+", "push"},
 		Run: func(cmd *cobra.Command, args []string) {
 			if err := runCreate(cmd, rf, args); err != nil {
 				poop.HitFan(err)
@@ -29,7 +31,21 @@ func runCreate(cmd *cobra.Command, rf *rootFlags, args []string) error {
 		return poop.Chain(err)
 	}
 
-	fmt.Println(r)
+	iter, err := r.Log(&git.LogOptions{})
+	if err != nil {
+		return poop.Chain(err)
+	}
+
+	for {
+		commit, err := iter.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return poop.Chain(err)
+		}
+
+		fmt.Println(commit.Hash.String())
+	}
 
 	return nil
 }
