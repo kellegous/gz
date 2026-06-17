80d4d5e28253b3408a92406e9e1bce45574ecf42
Kelly Norton <kellegous@gmail.com>
2026-06-17 06:37:20 -0400

    working on rebase

--- rs/src/git.rs
+++ rs/src/git.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use git2::{BranchType, Repository, build};
+use std::process::Command;
 
 pub fn checkout(repo: &Repository, name: &str) -> Result<()> {
     let branch = repo.find_branch(name, BranchType::Local)?;
@@ -13,5 +14,22 @@ pub fn checkout(repo: &Repository, name: &str) -> Result<()> {
 pub fn delete_branch(repo: &Repository, name: &str) -> Result<()> {
     let mut branch = repo.find_branch(name, BranchType::Local)?;
     branch.delete()?;
+    Ok(())
+}
+
+pub fn pull(repo: &Repository, name: &str) -> Result<()> {
+    // uses git directly mainly to piggyback on any credentials helpers
+    // that the user might have set up.
+    let workdir = repo
+        .workdir()
+        .ok_or_else(|| anyhow::anyhow!("bare repository"))?;
+    checkout(repo, name)?;
+    let status = Command::new("git")
+        .current_dir(workdir)
+        .args(["pull", "origin", name])
+        .status()?;
+    if !status.success() {
+        anyhow::bail!("git pull origin {name} failed");
+    }
     Ok(())
 }
--- rs/src/rebase.rs
+++ rs/src/rebase.rs
@@ -1,6 +1,7 @@
-use crate::store;
+use crate::{git, model::Branch, store};
 use anyhow::Result;
 use clap::Parser;
+use git2::Repository;
 use std::str::FromStr;
 
 #[derive(Debug, Parser)]
@@ -9,6 +10,13 @@ pub struct Args {
     at_root: AtRoot,
 }
 
+// TODO(kellegous): There are three strategies possible here:
+// 1. do nothing to the root and do not update the first branch to the
+//    HEAD of root. This is like a pinned chain.
+// 2. fetch the root, fast-forward it, rebase the chain. This is what
+//    gs rebase does.
+// 3. do nothing to the root, but update the first branch to the HEAD of
+//    root.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum AtRoot {
     Pull,
@@ -34,7 +42,18 @@ pub fn run(args: Args) -> Result<()> {
     let git_root =
         store::find_git_root()?.ok_or_else(|| anyhow::anyhow!("not in a git repository"))?;
 
-    let (mut _store, _store_path) = store::load_store_git_root(&git_root)?;
+    let (store, _store_path) = store::load_store_git_root(&git_root)?;
+
+    let repo = Repository::open(&git_root)?;
+    let head = repo.head()?;
+
+    if let Some(current) = store.get_branch(head.shorthand()?) {
+        // TODO(kellegous): write the chain into a recovery file
+        let chain = load_chain(&store, current)?;
+        println!("{:?}", chain);
+    } else {
+        update_root(&repo, head.shorthand()?, args.at_root)?;
+    }
 
     // Sketch this out.
     // 1. We will build a path to the root.
@@ -45,7 +64,25 @@ pub fn run(args: Args) -> Result<()> {
     //    c. if not, we will do a rebase --onto parent HEAD~N
     //    d. if there is a merge conflict, we will stop and do what?
 
-    // rebase --continue and --abort should probably be available.
+    // we will likely need to write a state file for the node that is being rebased
+    // otherwise we will not know how to continue when a merge conflict happens.
 
     Ok(())
+}
+
+fn update_root(repo: &Repository, name: &str, strategy: AtRoot) -> Result<()> {
+    if strategy == AtRoot::Pull {
+        git::pull(repo, name)?;
+    }
+    Ok(())
+}
+
+fn load_chain(store: &store::Store, current: &Branch) -> Result<Vec<Branch>> {
+    let mut chain = vec![current.clone()];
+    let mut current = current;
+    while let Some(parent) = store.get_branch(current.parent().name()) {
+        chain.push(parent.clone());
+        current = parent;
+    }
+    Ok(chain)
 }
